e22745a6ad07bd3da7e4fdcab7907f0dc984ad72
John Stratton <strattja@whitman.edu>
2023-01-07 15:59:52 -0800

    Full tiling of MM algorithm

--- mmul.cpp
+++ mmul.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 const size_t SIZE = 1024;
+const size_t TILE = 16;
+
 typedef Matrix& MatrixRef;
 
 void MultiplyMatrices(const MatrixRef a, const MatrixRef b, MatrixRef c) {
-  for (size_t i = 0; i < SIZE; i++) {
-    for (size_t j = 0; j < SIZE; j++) {
-      for (size_t k = 0; k < SIZE; k++) {
-        c[i][j] += a[i][k] * b[j][k];
+  for (size_t i_out = 0; i_out < SIZE; i_out+=TILE) {
+    for (size_t j_out = 0; j_out < SIZE; j_out+=TILE) {
+      for (size_t k_out = 0; k_out < SIZE; k_out+=TILE) {
+        for (size_t i = i_out; i < i_out+TILE; i++) {
+          for (size_t j = j_out; j < j_out+TILE; j++) {
+            for (size_t k = k_out; k < k_out+TILE; k++) {
+              c[i][j] += a[i][k] * b[j][k];
+            }
+          }
+        }
       }
     }
   }
